7046cc5d478e5ea57745acbf7a0a688802c12090
cameron.ellis <cameronellis@pop-os.localdomain>
2022-06-14 14:25:51 -0600

    initial commit

---
+++ src/main/java/com/ellis/spyder/Spyder.java
@@ -0,0 +1,29 @@
+package com.ellis.spyder;
+
+/*******************************************************************************
+*
+*<b>Title:</b> Spyder.java
+*<b>Project:</b> spyder
+*<b>Description:</b> ~~!!CHANGE ME!!~~
+*<b>Copyright:</b> Copyright (c) 2022
+*<b>Company:</b> Silicon Mountain Technologies
+*@author Cameron Ellis
+*@version 3.1
+*@since Jun 14, 2022
+*<b>updates:</b>
+*
+******************************************************************************/
+
+public class Spyder {
+	public static void main(String[] args) {
+		System.out.println("spyder");
+		
+		Spyder spyder1 = new Spyder();
+		
+		spyder1.spy();
+	}
+	
+	public void spy() {
+		System.out.println("this is also the spyder");
+	}
+}
---
+++ src/test/java/com/ellis/spyder/SpyderTest.java
@@ -0,0 +1,26 @@
+package com.ellis.spyder;
+
+import org.junit.jupiter.api.Test;
+
+/*******************************************************************************
+*
+*<b>Title:</b> SpyderTest.java
+*<b>Project:</b> spyder
+*<b>Description:</b> ~~!!CHANGE ME!!~~
+*<b>Copyright:</b> Copyright (c) 2022
+*<b>Company:</b> Silicon Mountain Technologies
+*@author Cameron Ellis
+*@version 3.1
+*@since Jun 14, 2022
+*<b>updates:</b>
+*
+******************************************************************************/
+
+public class SpyderTest {
+
+	@Test
+	public void testSpy() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
